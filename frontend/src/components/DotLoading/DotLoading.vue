868219437e4667c364d348f036a4694180346a68
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-10-17 19:30:42 -0300

    feat(components/DotLoading): create loading component

---
+++ frontend/src/components/DotLoading/DotLoading.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dot-loading">
+        <span class="dot one">X</span>
+        <span class="dot two">O</span>
+        <span class="dot three">X</span>
+        <span class="dot four">O</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+@import './DotLoading.scss'
+</style>
