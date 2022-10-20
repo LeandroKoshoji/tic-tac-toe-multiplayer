64bf17dec7bb695fc3da8c2a6facc6c987789549
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-10-20 13:18:44 -0300

    fix: fix socket io connections

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const socketio = require('socket.io')
+const { Server } = require('socket.io')
 const http = require('http')
 const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+app.use(cors())
+
 const server = http.createServer(app)
-const io = socketio(server, {
+const io = new Server(server, {
     cors: {
         origin: "http://localhost:8080",
     }
@@ -17,8 +19,12 @@ io.on('connection', (socket)=> {
     const socketUser = socket.id
     console.log(`${socketUser} connected`)
 
+    
+    
 
-    socket.on('disconnect', () => console.log(`${socketUser} disconnected`))
+    socket.on('disconnect', () => {
+        console.log(`${socketUser} disconnected`)
+    })
 })
 
 server.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`))
