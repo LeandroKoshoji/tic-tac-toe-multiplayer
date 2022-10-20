cffa6343a1f4bf85105e8a63076fde8cf3343812
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-10-20 13:08:45 -0300

    feat(views/OnlineBoardView):  create view scaffold

---
+++ frontend/src/views/BoardView/OnlineBoardView/OnlineBoardView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>onlineGame</h1>
+    </div>
+</template>
+
+<script setup>
+import { io } from "socket.io-client";
+
+const socket = io('http://localhost:3000')
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
