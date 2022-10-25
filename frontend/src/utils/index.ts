5034766d03c47bc711c7b49d53b27415b08ba722
Leandro Koshoji <leandro.koshoji@gmail.com>
2022-10-24 21:53:21 -0300

    feat(utils): create socket events enums

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -3,3 +3,17 @@ export const generateID = (): string => {
     const id = uuidv4()
     return id
 }
+
+export const SocketEmitEvents = Object.freeze({
+    CHECK_ROOM: 'check_room',
+    START_GAME: 'start_game',
+    PLAYER_MOVE: 'player_move',
+    REMATCH: 'rematch'
+})
+
+export const SocketOnEvents = Object.freeze({
+    READY_TO_START: 'ready_to_start',
+    GAME_UPDATE: 'game_update',
+    START_REMATCH_GAME: 'start_rematch_game',
+    PLAYER_DISCONNECTED: 'player_disconnected'
+})
